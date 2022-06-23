637db779406da5497254d22bc014e4a383966e70
Pavel Shvedko <pshvedko@gmail.com>
2022-06-23 21:04:09 +0300

    +1

--- model/codec_person_test.go
+++ model/codec_person_test.go
@@ -19,7 +19,7 @@ func TestCodecPerson_String(t *testing.T) {
 		{
 			name:   "",
 			fields: fields{},
-			want:   "",
+			want:   "{id=00000000-0000-0000-0000-000000000000 person={name= name1=<nil> name2=<nil> age=0 size=<nil> weight=<nil> id=00000000-0000-0000-0000-000000000000 sid=<nil> pid=<nil> url=<nil> time=0001-01-01 00:00:00 +0000 UTC date=<nil> date2=<nil> ok=false ok1=<nil> ok2=<nil> fruit=[] fruit2=[] fruit3=<nil>}}",
 		},
 	}
 	for _, tt := range tests {
